fadeab076116a36c109308ad3b5b0724fe203505
Dave Lucia <davelucianyc@gmail.com>
2022-02-27 10:33:52 -0500

    records can parse, fields, errors

--- lib/avro_ex/schema/schema_decode_error.ex
+++ lib/avro_ex/schema/schema_decode_error.ex
@@ -1,8 +1,30 @@
 defmodule AvroEx.Schema.DecodeError do
   defexception [:message]
 
-  def new(values) do
-    message = "Failed to decode schema for #{inspect(values[:reason])} data=#{inspect(values[:data])}"
+  def new({:unrecognized_fields, keys, type, data}) do
+    qualifier =
+      case keys do
+        [_] -> "key"
+        _ -> "keys"
+      end
+
+    message =
+      "Unrecognized schema #{qualifier} #{Enum.map_join(keys, ", ", &surround(&1, "`"))} for #{inspect(type)} in #{inspect(data)}"
+
     %__MODULE__{message: message}
+  end
+
+  def new({:missing_required, key, data}) do
+    message = "Schema missing required key #{surround(to_string(key), "`")} in #{inspect(data)}"
+    %__MODULE__{message: message}
+  end
+
+  def new({:invalid_format, data}) do
+    message = "Invalid schema format #{inspect(data)}"
+    %__MODULE__{message: message}
+  end
+
+  defp surround(string, value) do
+    value <> string <> value
   end
 end
--- test/schema_parser_test.exs
+++ test/schema_parser_test.exs
@@ -30,7 +30,12 @@ defmodule AvroEx.Schema.ParserTest do
         p_atom = String.to_atom(p)
 
         assert %Schema{schema: schema, context: context} =
-                 Parser.parse!(%{"type" => p, "a" => 1, "logicalType" => "timestamp-millis", "name" => "complex"})
+                 Parser.parse!(%{
+                   "type" => p,
+                   "a" => 1,
+                   "logicalType" => "timestamp-millis",
+                   "name" => "complex"
+                 })
 
         assert %Primitive{
                  type: ^p_atom,
@@ -42,11 +47,13 @@ defmodule AvroEx.Schema.ParserTest do
     end
 
     test "invalid primitives raise a DecodeError" do
-      assert_raise AvroEx.Schema.DecodeError, "Failed to decode schema for :invalid_format data=\"nope\"", fn ->
-        Parser.parse!("nope")
-      end
+      assert_raise AvroEx.Schema.DecodeError,
+                   "Invalid schema format \"nope\"",
+                   fn ->
+                     Parser.parse!("nope")
+                   end
 
-      message = "Failed to decode schema for :invalid_format data=%{\"type\" => \"nada\"}"
+      message = "Invalid schema format %{\"type\" => \"nada\"}"
 
       assert_raise AvroEx.Schema.DecodeError, message, fn ->
         Parser.parse!(%{"type" => "nada"})
@@ -73,6 +80,69 @@ defmodule AvroEx.Schema.ParserTest do
                  %Record.Field{name: "last", type: %Primitive{type: :string}}
                ]
              }
+
+      # TODO figure out context
+      # assert context == %Context{}
+    end
+
+    test "records can have fields that are logicalTypes" do
+      assert %Schema{schema: schema, context: context} =
+               Parser.parse!(%{
+                 "type" => "record",
+                 "name" => "analytics",
+                 "fields" => [
+                   %{
+                     "name" => "timestamp",
+                     "type" => %{"type" => "string", "logicalType" => "timestamp-millis"}
+                   }
+                 ]
+               })
+
+      assert schema == %Record{
+               name: "analytics",
+               fields: [
+                 %Record.Field{
+                   name: "timestamp",
+                   type: %Primitive{type: :string, metadata: %{"logicalType" => "timestamp-millis"}}
+                 }
+               ]
+             }
+
+      # TODO figure out context
+      # assert context == %Context{}
+    end
+
+    test "creating a record without a name will raise" do
+      message =
+        "Schema missing required key `name` in %{\"fields\" => [%{\"name\" => \"key\", \"type\" => \"long\"}], \"type\" => \"record\"}"
+
+      assert_raise AvroEx.Schema.DecodeError, message, fn ->
+        Parser.parse!(%{
+          "type" => "record",
+          "fields" => [
+            %{"name" => "key", "type" => "long"}
+          ]
+        })
+      end
+    end
+
+    test "trying to use a logicalType on a field will raise" do
+      message =
+        "Unrecognized schema key `logicalType` for AvroEx.Schema.Record.Field in %{\"logicalType\" => \"timestamp-millis\", \"name\" => \"timestamp\", \"type\" => \"long\"}"
+
+      assert_raise AvroEx.Schema.DecodeError, message, fn ->
+        Parser.parse!(%{
+          "type" => "record",
+          "name" => "analytics",
+          "fields" => [
+            %{"name" => "timestamp", "type" => "long", "logicalType" => "timestamp-millis"}
+          ]
+        })
+      end
+    end
+
+    test "aliases" do
+      flunk()
     end
   end
 end
