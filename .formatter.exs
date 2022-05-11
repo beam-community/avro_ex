2f9d4a46d64ddf92e0e8fa8d9db9a889cca8ac6c
Benjamin Milde <benni@kobrakai.de>
2022-05-11 21:54:02 +0200

    Add property test for primitive and non named complex types

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 # Used by "mix format"
 [
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  import_deps: [:typed_struct],
+  import_deps: [:typed_struct, :stream_data],
   line_length: 120
 ]
