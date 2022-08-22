d012a83fcb4d636513c1565a7b1da4652a46a5fe
Zander Erasmus <zerasmus@corvusenergy.com>
2022-08-22 10:16:47 -0700

    Use alias instead of property

--- test/object_container_encode_test.exs
+++ test/object_container_encode_test.exs
@@ -1,13 +1,13 @@
 defmodule AvroEx.ObjectContainer.Encode.Test do
   use ExUnit.Case, async: true
 
-  @test_module AvroEx.ObjectContainer
+  alias AvroEx.ObjectContainer
 
   describe "encode file header" do
     test "new containers have different sync bytes" do
       containers =
         for _ <- 1..10 do
-          @test_module.new(nil)
+          ObjectContainer.new(nil)
         end
 
       for container <- containers do
@@ -25,13 +25,13 @@ defmodule AvroEx.ObjectContainer.Encode.Test do
 
       containers =
         for codec <- codecs do
-          @test_module.new(AvroEx.decode_schema!(~S("null")), codec: codec)
+          ObjectContainer.new(AvroEx.decode_schema!(~S("null")), codec: codec)
         end
 
       headers =
         for container <- containers do
-          headerdata = @test_module.encode_file_header!(container)
-          AvroEx.decode!(@test_module.file_header_schema, headerdata)
+          headerdata = ObjectContainer.encode_file_header!(container)
+          AvroEx.decode!(ObjectContainer.file_header_schema, headerdata)
         end
 
       for {header, codec} <- Enum.zip(headers, codecs) do
@@ -40,46 +40,46 @@ defmodule AvroEx.ObjectContainer.Encode.Test do
     end
 
     test "default codec is null" do
-      container = @test_module.new(AvroEx.decode_schema!(~S("null")))
-      headerdata = @test_module.encode_file_header!(container)
-      header = AvroEx.decode!(@test_module.file_header_schema, headerdata)
+      container = ObjectContainer.new(AvroEx.decode_schema!(~S("null")))
+      headerdata = ObjectContainer.encode_file_header!(container)
+      header = AvroEx.decode!(ObjectContainer.file_header_schema, headerdata)
       assert header["meta"]["avro.codec"] == "null"
     end
 
     test "schema is stored in the file header metadata" do
-      container = @test_module.new(AvroEx.decode_schema!(~S("null")))
-      headerdata = @test_module.encode_file_header!(container)
-      header = AvroEx.decode!(@test_module.file_header_schema, headerdata)
+      container = ObjectContainer.new(AvroEx.decode_schema!(~S("null")))
+      headerdata = ObjectContainer.encode_file_header!(container)
+      header = AvroEx.decode!(ObjectContainer.file_header_schema, headerdata)
       assert header["meta"]["avro.schema"] == "{\"type\":\"null\"}"
     end
 
     test "user metadata is stored in the file header metadata" do
-      container = @test_module.new(AvroEx.decode_schema!(~S("null")), meta: %{first_time: "12345678"})
-      headerdata = @test_module.encode_file_header!(container)
-      header = AvroEx.decode!(@test_module.file_header_schema, headerdata)
+      container = ObjectContainer.new(AvroEx.decode_schema!(~S("null")), meta: %{first_time: "12345678"})
+      headerdata = ObjectContainer.encode_file_header!(container)
+      header = AvroEx.decode!(ObjectContainer.file_header_schema, headerdata)
       assert header["meta"]["first_time"] == "12345678"
     end
 
     test "user metadata does not prevent schema and codec from being written preoperly" do
-      container = @test_module.new(AvroEx.decode_schema!(~S("null")), meta: %{first_time: "12345678"})
-      headerdata = @test_module.encode_file_header!(container)
-      header = AvroEx.decode!(@test_module.file_header_schema, headerdata)
+      container = ObjectContainer.new(AvroEx.decode_schema!(~S("null")), meta: %{first_time: "12345678"})
+      headerdata = ObjectContainer.encode_file_header!(container)
+      header = AvroEx.decode!(ObjectContainer.file_header_schema, headerdata)
       assert header["meta"]["avro.codec"] == "null"
       assert header["meta"]["avro.schema"] == "{\"type\":\"null\"}"
     end
 
     test "magic matches standard" do
-      container = @test_module.new(AvroEx.decode_schema!(~S("null")))
-      headerdata = @test_module.encode_file_header!(container)
-      header = AvroEx.decode!(@test_module.file_header_schema, headerdata)
+      container = ObjectContainer.new(AvroEx.decode_schema!(~S("null")))
+      headerdata = ObjectContainer.encode_file_header!(container)
+      header = AvroEx.decode!(ObjectContainer.file_header_schema, headerdata)
       assert header["magic"] == <<"Obj", 1>>
     end
   end
 
   test "encode block header" do
     # TODO: property based test makes more sense
-    encoded_header = @test_module.encode_block_header!(100, 5000)
-    header = AvroEx.decode!(@test_module.block_header_schema, encoded_header)
+    encoded_header = ObjectContainer.encode_block_header!(100, 5000)
+    header = AvroEx.decode!(ObjectContainer.block_header_schema, encoded_header)
     assert header["num_objects"] == 100
     assert header["num_bytes"] == 5000
   end
