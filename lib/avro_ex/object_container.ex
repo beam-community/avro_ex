5426a95c8a93937a021b5232ddb50b893cdd97de
Zander Erasmus <zerasmus@corvusenergy.com>
2022-10-24 10:24:56 -0700

    Decode file container objects Implement all the functions that go into decoding parts of the object container

--- lib/avro_ex/object_container.ex
+++ lib/avro_ex/object_container.ex
@@ -94,7 +94,6 @@ defmodule AvroEx.ObjectContainer do
     end
   end
 
-
   defp get_schema(%{"avro.schema" => schema}), do: {:ok, schema}
   defp get_schema(_), do: {:error, %AvroEx.DecodeError{message: "Invalid or missing schema in file header"}}
   defp get_codec(%{"avro.codec" => codec}), do: {:ok, codec}
@@ -121,5 +120,80 @@ defmodule AvroEx.ObjectContainer do
          sync: decoded_header["sync"]
        }, rest}
     end
+  end
+
+  defp check_block_header(%{"num_objects" => num_objects, "num_bytes" => num_bytes})
+       when is_integer(num_objects) and num_objects >= 0 and is_integer(num_bytes) and num_bytes >= 0,
+       do: {:ok, %{num_objects: num_objects, num_bytes: num_bytes}}
+
+  defp check_block_header(%{num_objects: num_objects, num_bytes: num_bytes})
+       when is_integer(num_objects) and num_objects >= 0 and is_integer(num_bytes) and num_bytes >= 0,
+       do: {:ok, %{num_objects: num_objects, num_bytes: num_bytes}}
+
+  defp check_block_header(_), do: {:error, %AvroEx.DecodeError{message: "Invalid block header"}}
+
+  def decode_block_header(data) do
+    with {:ok, header, rest} <- decode_with_rest(@bh_schema, data),
+         {:ok, checked_header} <- check_block_header(header) do
+      {:ok, checked_header, rest}
+    end
+  end
+
+  def check_block_footer(%__MODULE__{sync: sync}, <<read_sync::128, rest::binary>>) when sync == read_sync,
+    do: {:ok, rest}
+
+  def check_block_footer(_, <<_::128, _::binary>>), do: {:error, %AvroEx.DecodeError{message: "Invalid sync bytes"}}
+
+  defp do_decode_block_objects(file_header, data, objects \\ [])
+  defp do_decode_block_objects(_file_header, <<>>, objects), do: {:ok, Enum.reverse(objects)}
+
+  defp do_decode_block_objects(%__MODULE__{} = file_header, data, objects) do
+    with {:ok, object, rest} <- decode_with_rest(file_header.schema, data) do
+      do_decode_block_objects(file_header, rest, [object | objects])
+    end
+  end
+
+  defp get_object_data(num_bytes, data) do
+    with <<object_data::binary-size(num_bytes), rest::binary>> <- data do
+      {:ok, object_data, rest}
+    else
+      _ -> {:error, %AvroEx.DecodeError{message: "Not enough bytes for block objects"}}
+    end
+  end
+
+  defp check_num_objects(objects, num_objects) when length(objects) == num_objects, do: :ok
+  defp check_num_objects(_, _), do: {:error, %AvroEx.DecodeError{message: "Invalid number of objects"}}
+
+  def decode_block_objects(file_header, block_header, data) do
+    with {:ok, %{num_objects: num_objects, num_bytes: num_bytes}} <- check_block_header(block_header),
+         {:ok, object_data, rest} <- get_object_data(num_bytes, data),
+         {:ok, objects} <- do_decode_block_objects(file_header, object_data),
+         :ok <- check_num_objects(objects, num_objects) do
+      {:ok, objects, rest}
+    end
+  end
+
+  def decode_block(file_header, data) do
+    with {:ok, block_header, rest} <- decode_block_header(data),
+         {:ok, objects, rest} <- decode_block_objects(file_header, block_header, rest),
+         {:ok, rest} <- check_block_footer(file_header, rest) do
+      {:ok, objects, rest}
+    end
+  end
+
+  defp do_decode_blocks(file_header, data, objects \\ [])
+  defp do_decode_blocks(_file_header, <<>>, objects), do: {:ok, objects |> Enum.reverse() |> List.flatten()}
+
+  defp do_decode_blocks(file_header, data, objects) do
+    with {:ok, new_objects, rest} <- decode_block(file_header, data) do
+      do_decode_blocks(file_header, rest, [new_objects | objects])
+    end
+  end
+
+  def decode_blocks(file_header, data) do
+    do_decode_blocks(file_header, data)
+  end
+
+  def decode_file(data) do
   end
 end
