4e4bd866bf9c9d9a6bb72cbb44d8052b68b41f3e
Benjamin Milde <benni@kobrakai.de>
2023-02-06 19:56:32 +0100

    Allow encoder to encode block header with optional byte size (#83)

--- lib/avro_ex/encode.ex
+++ lib/avro_ex/encode.ex
@@ -14,33 +14,34 @@ defmodule AvroEx.Encode do
   @seconds_in_day 24 * 60 * 60
 
   @doc false
-  @spec encode(Schema.t(), term) :: {:ok, AvroEx.encoded_avro()} | {:error, EncodeError.t() | Exception.t()}
-  def encode(%Schema{context: %Context{} = context, schema: schema}, data) do
+  @spec encode(Schema.t(), term, keyword()) :: {:ok, AvroEx.encoded_avro()} | {:error, EncodeError.t() | Exception.t()}
+  def encode(%Schema{context: %Context{} = context, schema: schema}, data, opts \\ []) do
     try do
-      {:ok, do_encode(schema, context, data)}
+      {:ok, do_encode(schema, context, data, opts)}
     catch
       :throw, %EncodeError{} = e -> {:error, e}
     end
   end
 
-  defp do_encode(%Reference{type: type}, %Context{} = context, data) do
-    do_encode(Context.lookup(context, type), context, data)
+  defp do_encode(%Reference{type: type}, %Context{} = context, data, opts) do
+    do_encode(Context.lookup(context, type), context, data, opts)
   end
 
-  defp do_encode(%Primitive{type: :boolean}, %Context{}, true), do: <<1::8>>
-  defp do_encode(%Primitive{type: :boolean}, %Context{}, false), do: <<0::8>>
-  defp do_encode(%Primitive{type: :null}, %Context{}, nil), do: <<>>
+  defp do_encode(%Primitive{type: :boolean}, %Context{}, true, _), do: <<1::8>>
+  defp do_encode(%Primitive{type: :boolean}, %Context{}, false, _), do: <<0::8>>
+  defp do_encode(%Primitive{type: :null}, %Context{}, nil, _), do: <<>>
 
-  defp do_encode(%Primitive{type: :float}, %Context{}, float) when is_float(float),
+  defp do_encode(%Primitive{type: :float}, %Context{}, float, _) when is_float(float),
     do: <<float::little-float-size(32)>>
 
-  defp do_encode(%Primitive{type: :double}, %Context{}, double) when is_float(double),
+  defp do_encode(%Primitive{type: :double}, %Context{}, double, _) when is_float(double),
     do: <<double::little-float-size(64)>>
 
   defp do_encode(
          %Primitive{type: :int, metadata: %{"logicalType" => "date"}} = schema,
          %Context{},
-         %Date{} = date
+         %Date{} = date,
+         _
        ) do
     date
     |> DateTime.new!(~T[00:00:00])
@@ -52,7 +53,8 @@ defmodule AvroEx.Encode do
   defp do_encode(
          %Primitive{type: :long, metadata: %{"logicalType" => "timestamp-nanos"}} = schema,
          %Context{},
-         %DateTime{} = dt
+         %DateTime{} = dt,
+         _
        ) do
     dt
     |> DateTime.to_unix(:nanosecond)
@@ -62,7 +64,8 @@ defmodule AvroEx.Encode do
   defp do_encode(
          %Primitive{type: :long, metadata: %{"logicalType" => "timestamp-micros"}} = schema,
          %Context{},
-         %DateTime{} = dt
+         %DateTime{} = dt,
+         _
        ) do
     dt
     |> DateTime.to_unix(:microsecond)
@@ -72,7 +75,8 @@ defmodule AvroEx.Encode do
   defp do_encode(
          %Primitive{type: :long, metadata: %{"logicalType" => "timestamp-millis"}} = schema,
          %Context{},
-         %DateTime{} = dt
+         %DateTime{} = dt,
+         _
        ) do
     dt
     |> DateTime.to_unix(:millisecond)
@@ -82,7 +86,8 @@ defmodule AvroEx.Encode do
   defp do_encode(
          %Primitive{type: :long, metadata: %{"logicalType" => "time-micros"}} = schema,
          %Context{},
-         %Time{} = dt
+         %Time{} = dt,
+         _
        ) do
     {:ok, midnight} = Time.new(0, 0, 0)
 
@@ -94,7 +99,8 @@ defmodule AvroEx.Encode do
   defp do_encode(
          %Primitive{type: :int, metadata: %{"logicalType" => "time-millis"}} = schema,
          %Context{},
-         %Time{} = dt
+         %Time{} = dt,
+         _
        ) do
     {:ok, midnight} = Time.new(0, 0, 0)
 
@@ -103,92 +109,92 @@ defmodule AvroEx.Encode do
     |> encode_integer(schema)
   end
 
-  defp do_encode(%Primitive{type: :long} = schema, %Context{}, long) when is_integer(long) do
+  defp do_encode(%Primitive{type: :long} = schema, %Context{}, long, _) when is_integer(long) do
     encode_integer(long, schema)
   end
 
-  defp do_encode(%Primitive{type: :int} = schema, %Context{}, integer)
+  defp do_encode(%Primitive{type: :int} = schema, %Context{}, integer, _)
        when is_integer(integer) do
     encode_integer(integer, schema)
   end
 
-  defp do_encode(%Primitive{type: :string} = primitive, %Context{} = context, atom)
+  defp do_encode(%Primitive{type: :string} = primitive, %Context{} = context, atom, opts)
        when is_atom(atom) and not (is_nil(atom) or is_boolean(atom)) do
-    do_encode(primitive, context, to_string(atom))
+    do_encode(primitive, context, to_string(atom), opts)
   end
 
-  defp do_encode(%Primitive{type: :string}, %Context{} = context, str) when is_binary(str) do
+  defp do_encode(%Primitive{type: :string}, %Context{} = context, str, opts) when is_binary(str) do
     if String.valid?(str) do
-      do_encode(%Primitive{type: :bytes}, context, str)
+      do_encode(%Primitive{type: :bytes}, context, str, opts)
     else
       error({:invalid_string, str, context})
     end
   end
 
-  defp do_encode(%Primitive{type: :bytes}, %Context{} = context, bin) when is_binary(bin) do
+  defp do_encode(%Primitive{type: :bytes}, %Context{} = context, bin, opts) when is_binary(bin) do
     byte_count = :erlang.size(bin)
-    size = do_encode(%Primitive{type: :long}, context, byte_count)
+    size = do_encode(%Primitive{type: :long}, context, byte_count, opts)
     size <> bin
   end
 
-  defp do_encode(%Fixed{size: size}, %Context{}, bin)
+  defp do_encode(%Fixed{size: size}, %Context{}, bin, _)
        when is_binary(bin) and byte_size(bin) == size do
     bin
   end
 
-  defp do_encode(%Fixed{} = fixed, %Context{} = context, bin) when is_binary(bin) do
+  defp do_encode(%Fixed{} = fixed, %Context{} = context, bin, _) when is_binary(bin) do
     error({:incorrect_fixed_size, fixed, bin, context})
   end
 
-  defp do_encode(%Record{fields: fields}, %Context{} = context, record) when is_map(record) do
+  defp do_encode(%Record{fields: fields}, %Context{} = context, record, opts) when is_map(record) do
     record =
       Map.new(record, fn
         {k, v} when is_binary(k) -> {k, v}
         {k, v} when is_atom(k) -> {to_string(k), v}
       end)
 
-    Enum.map_join(fields, &do_encode(&1, context, record[&1.name]))
+    Enum.map_join(fields, &do_encode(&1, context, record[&1.name], opts))
   end
 
-  defp do_encode(%Field{type: type, default: default}, %Context{} = context, nil) do
-    do_encode(type, context, default)
+  defp do_encode(%Field{type: type, default: default}, %Context{} = context, nil, opts) do
+    do_encode(type, context, default, opts)
   end
 
-  defp do_encode(%Field{type: type}, %Context{} = context, value) do
-    do_encode(type, context, value)
+  defp do_encode(%Field{type: type}, %Context{} = context, value, opts) do
+    do_encode(type, context, value, opts)
   end
 
-  defp do_encode(%Union{possibilities: possibilities} = schema, %Context{} = context, {name, value} = original) do
+  defp do_encode(%Union{possibilities: possibilities} = schema, %Context{} = context, {name, value} = original, opts) do
     index =
       Enum.find_index(possibilities, fn
         %{name: ^name} = possible_schema -> Schema.encodable?(possible_schema, context, value)
         _ -> false
       end)
 
-    do_encode_union(schema, context, value, original, index)
+    do_encode_union(schema, context, value, original, index, opts)
   end
 
-  defp do_encode(%Union{possibilities: possibilities} = schema, %Context{} = context, value) do
+  defp do_encode(%Union{possibilities: possibilities} = schema, %Context{} = context, value, opts) do
     index =
       Enum.find_index(possibilities, fn possible_schema ->
         Schema.encodable?(possible_schema, context, value)
       end)
 
-    do_encode_union(schema, context, value, value, index)
+    do_encode_union(schema, context, value, value, index, opts)
   end
 
-  defp do_encode(%AvroEx.Schema.Map{values: values}, %Context{} = context, map) when is_map(map) do
+  defp do_encode(%AvroEx.Schema.Map{values: values}, %Context{} = context, map, opts) when is_map(map) do
     case map_size(map) do
       0 ->
         <<0>>
 
       size ->
-        acc = do_encode(%Primitive{type: :long}, context, size)
+        acc = do_encode(%Primitive{type: :long}, context, size, opts)
 
         encoded_map =
           Enum.reduce(map, acc, fn {k, v}, acc ->
-            key = do_encode(%Primitive{type: :string}, context, k)
-            value = do_encode(values, context, v)
+            key = do_encode(%Primitive{type: :string}, context, k, opts)
+            value = do_encode(values, context, v, opts)
 
             acc <> key <> value
           end)
@@ -197,47 +203,61 @@ defmodule AvroEx.Encode do
     end
   end
 
-  defp do_encode(%Array{items: items}, %Context{} = context, data) when is_list(data) do
+  defp do_encode(%Array{items: items}, %Context{} = context, data, opts) when is_list(data) do
     case length(data) do
       0 ->
         <<0>>
 
       size ->
-        acc = do_encode(%Primitive{type: :long}, context, size)
-
-        encoded_array =
-          Enum.reduce(data, acc, fn v, acc ->
-            value = do_encode(items, context, v)
+        array_payload =
+          Enum.reduce(data, <<>>, fn v, acc ->
+            value = do_encode(items, context, v, opts)
 
             acc <> value
           end)
 
-        encoded_array <> <<0>>
+        header =
+          if Keyword.get(opts, :include_block_byte_size, false) do
+            negated_count = do_encode(%Primitive{type: :long}, context, -1 * size, opts)
+            byte_size = do_encode(%Primitive{type: :long}, context, byte_size(array_payload), opts)
+            negated_count <> byte_size
+          else
+            do_encode(%Primitive{type: :long}, context, size, opts)
+          end
+
+        header <> array_payload <> <<0>>
     end
   end
 
-  defp do_encode(%AvroEnum{} = enum, %Context{} = context, atom) when is_atom(atom) do
-    do_encode(enum, context, to_string(atom))
+  defp do_encode(%AvroEnum{} = enum, %Context{} = context, atom, opts) when is_atom(atom) do
+    do_encode(enum, context, to_string(atom), opts)
   end
 
-  defp do_encode(%AvroEnum{symbols: symbols} = enum, %Context{} = context, data) when is_binary(data) do
+  defp do_encode(%AvroEnum{symbols: symbols} = enum, %Context{} = context, data, opts) when is_binary(data) do
     if data in symbols do
       index = Enum.find_index(symbols, fn e -> e == data end)
-      do_encode(%Primitive{type: :long}, context, index)
+      do_encode(%Primitive{type: :long}, context, index, opts)
     else
       error({:invalid_symbol, enum, data, context})
     end
   end
 
-  defp do_encode(schema, context, data) do
+  defp do_encode(schema, context, data, _) do
     error({:schema_mismatch, schema, data, context})
   end
 
-  defp do_encode_union(%Union{possibilities: possibilities} = schema, %Context{} = context, value, original, index) do
+  defp do_encode_union(
+         %Union{possibilities: possibilities} = schema,
+         %Context{} = context,
+         value,
+         original,
+         index,
+         opts
+       ) do
     if index do
       schema = Enum.at(possibilities, index)
 
-      do_encode(%Primitive{type: :int}, context, index) <> do_encode(schema, context, value)
+      do_encode(%Primitive{type: :int}, context, index, opts) <> do_encode(schema, context, value, opts)
     else
       error({:schema_mismatch, schema, original, context})
     end
